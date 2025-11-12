4671bd8783809cdc8946bdfa7ead234a63296c42
Tatenda Carl Sakarombe <90087474+Sakaz22@users.noreply.github.com>
2025-11-12 20:01:22 +0100

    Automation Testing Practice 2

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -15,7 +15,22 @@ test('is clickable GUI', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter Name' }).click();
   await page.getByRole('textbox', { name: 'Enter Name' }).fill('Tatenda');
 
-  // Enter text into "Name" textbox.
+  // Enter text into "EMail" textbox.
   await page.getByRole('textbox', { name: 'Enter EMail' })
   await page.getByRole('textbox', { name: 'Enter EMail' }).fill('tats@gmail.com');
+
+  // Select gender using radio box.
+  await page.getByRole('radio', { name: 'Male', exact: true }).check();
+
+  // Select days using tick box.
+  await page.getByRole('checkbox', { name: 'Sunday' }).check();
+  await page.getByRole('checkbox', { name: 'Monday' }).check();
+  await page.getByRole('checkbox', { name: 'Tuesday' }).check();
+  await page.getByRole('checkbox', { name: 'Wednesday' }).check();
+  await page.getByRole('checkbox', { name: 'Thursday' }).check();
+  await page.getByRole('checkbox', { name: 'Friday' }).check();
+  await page.getByRole('checkbox', { name: 'Saturday' }).check();
+
+  // Select country using dropdown.
+  await page.getByLabel('Country:').selectOption('germany');
 });
